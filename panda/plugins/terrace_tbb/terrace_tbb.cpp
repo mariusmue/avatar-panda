ae21de0f0a3b99cf3960e4f09274a669f81a82e3
ChrisMcMStone <christopher-ms@hotmail.com>
2019-05-05 17:24:44 +0100

    Added support for extracting block trace based off ASID + address spaces (for whole-OS emulation)

--- panda/plugins/terrace_tbb/terrace_tbb.cpp
+++ panda/plugins/terrace_tbb/terrace_tbb.cpp
@@ -24,10 +24,13 @@ TBBBlocks blocks;
 target_ulong last_pc = 0; 
 unsigned int n = 0;
 
+target_ulong asid;
+target_ulong start_addr;
+target_ulong end_addr;
+
 }
 
-void write_entry(void)
-{
+void write_entry(void) {
 
     TBBBlock *b = blocks.add_basic_blocks();
     b->set_address(last_pc);
@@ -37,25 +40,37 @@ void write_entry(void)
 
 
 int after_block_exec_trace_tb(CPUState *env, TranslationBlock *tb) {
-    target_ulong pc = tb->pc;
 
-    if ( (pc != last_pc || n == UINT_MAX ) && n > 0) {
-        write_entry();
+    if( !asid || panda_current_asid(env) == asid ) {
+        if( !end_addr || ((tb->pc) < end_addr && (tb->pc) >= start_addr) ) {
+            target_ulong pc = tb->pc;
+
+            if ( (pc != last_pc || n == UINT_MAX ) && n > 0) {
+                write_entry();
+            }
+            else {
+                n++;
+            }
+            last_pc = pc;
+        }
     }
-    else {
-        n++;
-    }
-    last_pc = pc;
     return 0;
-
 }
+
 bool init_plugin(void *self) {
 
     panda_arg_list *args = panda_get_args("terrace_tbb");
 
     trace_file_name = panda_parse_string_opt(args, "trace_file",
             "basic_blocks.bin", "File to store traced BB addresses");
+    asid = panda_parse_ulong_opt(args, "asid", 0, 
+            "The address space ID for the target process");
 
+    start_addr = panda_parse_ulong_opt(args, "start_addr", 0, "base address of target binary");
+    uint32_t size_dec = panda_parse_uint32_opt(args, "bin_size", 0, "address space size (decimal)");
+    
+    if( size_dec ) end_addr = start_addr + size_dec;
+ 
     panda_cb pcb;
 
     pcb.after_block_exec = after_block_exec_trace_tb;
